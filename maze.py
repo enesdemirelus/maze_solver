1c58f02971cfa03d0642625a901e2b9e47224e93
enesdemirelus <enesdemirelus@gmail.com>
2025-03-27 16:18:29 -0500

    finished the game parts of the maze, random maze creation not finished yet

--- maze.py
+++ maze.py
@@ -3,7 +3,7 @@ import random
 
 pygame.init()
 
-WIDTH, HEIGHT = 15, 15
+WIDTH, HEIGHT = 10,10
 CELL_SIZE = 40
 SCREEN_WIDTH = WIDTH * CELL_SIZE
 SCREEN_HEIGHT = HEIGHT * CELL_SIZE
@@ -12,25 +12,40 @@ screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption('Maze')
 
 def create_maze(width, height):
-    maze = []
-    for y in range(height):
-        row = []
-        for x in range(width):
-            if x % 2 != 0 or y % 2 != 0:
-                row.append(1)
-            else:
-                row.append(0)
-        maze.append(row)
+    maze = [[1, 1, 0, 0, 1, 1, 1, 0, 0, 1],
+            [0, 1, 1, 1, 0, 0, 1, 1, 0, 1],
+            [1, 0, 0, 1, 1, 0, 0, 1, 1, 1],
+            [1, 0, 0, 0, 1, 1, 0, 0, 0, 1],
+            [1, 1, 1, 1, 0, 1, 1, 0, 1, 1],
+            [0, 1, 0, 0, 0, 0, 1, 0, 1, 0],
+            [1, 1, 1, 1, 1, 0, 1, 1, 1, 0],
+            [1, 0, 1, 0, 0, 0, 0, 0, 1, 0],
+            [1, 0, 1, 0, 1, 1, 1, 1, 1, 0],
+            [1, 0, 1, 1, 1, 0, 0, 0, 1, 1]]
+
+    # for y in range(height):
+    #     row = []
+    #     for x in range(width):
+    #         if x % 2 != 0 or y % 2 != 0:
+    #             row.append(1)
+    #         else:
+    #             row.append(0)
+    #     maze.append(row)
     return maze
 
 def draw_maze(maze):
     for y in range(HEIGHT):
         for x in range(WIDTH):
             rect = pygame.Rect(x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE, CELL_SIZE)
-            if maze[y][x] == 1:
+            if x == WIDTH - 1 and y == HEIGHT - 1:
+                pygame.draw.rect(screen, (255, 0, 21), rect)
+            elif x == 0 and y == 0:
+                pygame.draw.rect(screen, (255, 225, 0), rect)
+            elif maze[y][x] == 0:
                 pygame.draw.rect(screen, (0, 0, 0), rect)
             else:
                 pygame.draw.rect(screen, (255, 255, 255), rect)
+                
                 
 def move_player(player_pos, direction, maze):
     x, y = player_pos
@@ -38,18 +53,22 @@ def move_player(player_pos, direction, maze):
              pygame.K_DOWN: "DOWN", 
              pygame.K_LEFT: "LEFT",
              pygame.K_RIGHT: "RIGHT"}
+    
         
     try:    
-        if moves[direction] == "UP" and y >= 1: 
+        if moves[direction] == "UP" and y >= 1 and maze[y - 1][x] != 0 : 
             y -= 1   
-        elif moves[direction] == "DOWN" and y < HEIGHT - 1:
-            y += 1       
-        elif moves[direction] == "LEFT" and x >= 1:
+        elif moves[direction] == "DOWN" and y < HEIGHT - 1 and maze[y + 1][x] != 0:
+            y += 1  
+        elif moves[direction] == "LEFT" and x >= 1 and maze[y][x -1 ] != 0:
             x -= 1      
-        elif moves[direction] == "RIGHT" and x < WIDTH - 1:        
-            x += 1     
+        elif moves[direction] == "RIGHT" and x < WIDTH - 1 and maze[y][x + 1] != 0:        
+            x += 1    
+            
+        if x == WIDTH - 1 and y == HEIGHT - 1:
+            print("YOU WON!")
     except:
-        print("Wrong Key")
+        pass
         
         
     return (x, y)
